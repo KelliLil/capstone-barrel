a5dac2f98f842fff21368f00993fda5891110d68
Kelli Lilly <kelli.lilly05@gmail.com>
2023-01-31 14:25:18 -0600

    Connect to mongo

---
+++ app/user/controller.js
@@ -0,0 +1,14 @@
+import mongoose from "mongoose";
+import config from "../config.js";
+import User from "./User.js";
+
+mongoose.set("strictQuery", true);
+
+mongoose
+  .connect(config.getDbConn("test"))
+  .then(() => {
+    console.log("Connected to DB");
+  })
+  .catch((err) => {
+    console.log("Error connecting to DB", err);
+  });

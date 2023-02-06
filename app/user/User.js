6468b7cb3f87c44f9fc0a5582b295e7d6f044484
Kelli Lilly <kelli.lilly05@gmail.com>
2023-02-06 10:06:40 -0600

    Create login and password schema

--- app/user/User.js
+++ app/user/User.js
@@ -1,5 +1,8 @@
+import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
 import { model, Schema } from "mongoose";
 import groupSchema from "./group-schema.js";
+import config from "../config.js";
 
 const userSchema = new Schema({
   name: { type: String, required: true },
@@ -8,8 +11,37 @@ const userSchema = new Schema({
   dietRestrictions: { type: [String], required: true },
   groupsJoined: [groupSchema],
   groupsAdmins: [groupSchema],
-
-  versionKey: false,
 });
+
+userSchema.pre("save", async function (next) {
+  if (this.isModified("password")) {
+    const generatedSalt = await bcrypt.genSalt(config.saltRounds);
+    this.password = await bcrypt.hash(this.password, generatedSalt);
+  }
+
+  next();
+});
+
+userSchema.statics.login = async function (username, password) {
+  const user = await this.findOne({ username });
+
+  let isMatch = false;
+  if (user) {
+    isMatch = await bcrypt.compare(password, user.password);
+  }
+
+  return isMatch
+    ? jwt.sign(
+        {
+          user: {
+            id: user._id,
+            username: user.username,
+          },
+        },
+        config.jwtSecret,
+        { expiresIn: config.jwtExpiresIn }
+      )
+    : null;
+};
 
 export default model("User", userSchema);

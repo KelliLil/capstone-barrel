75d48855cb0aa4a5497385572bf6a1a35d8add00
Kelli Lilly <kelli.lilly05@gmail.com>
2023-02-06 11:45:59 -0600

    Add hashing to config

--- app/config.js
+++ app/config.js
@@ -4,6 +4,8 @@ dotenv.config();
 
 export default {
   dbConn: process.env.DB_CONN,
+  jwtExpiresIn: process.env.JWT_EXPIRES_IN,
   jwtSecret: process.env.JWT_SECRET,
   port: process.env.PORT || 3000,
+  saltRounds: Number(process.env.SALT_ROUNDS),
 };

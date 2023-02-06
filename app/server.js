34427eb182a66ce0a07a05023f02e371960451c5
Kelli Lilly <kelli.lilly05@gmail.com>
2023-02-06 10:17:35 -0600

    style: Set up server file

---
+++ app/server.js
@@ -0,0 +1,21 @@
+import express from "express";
+import config from "./config.js";
+import decodeUser from "./middleware/decode-user.js";
+import routes from "./routes.js";
+
+export default () => {
+  const app = express();
+
+  // * Middleware order matters!
+  app.use(express.json());
+  app.use(decodeUser);
+  app.use("/api/users/", routes);
+
+  app.use((_, res) => {
+    res.status(404).json({ message: "Not found" });
+  });
+
+  app.listen(config.port, () => {
+    console.info(`Server running on: http://localhost:${config.port}`);
+  });
+};

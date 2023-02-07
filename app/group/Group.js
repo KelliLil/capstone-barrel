901f14efab24f94a588a6223514c1491c9adc6bb
Kelli Lilly <kelli.lilly05@gmail.com>
2023-02-07 11:37:43 -0600

    refactor: add adminschema

--- app/group/Group.js
+++ app/group/Group.js
@@ -1,13 +1,20 @@
-import { model, Schema } from "mongoose";
-import userSchema from "./user/user-schema.js";
-import voteSchema from "./user/vote-schema.js";
+import { model, Schema, Types } from "mongoose";
+import { userSchema } from "../user/User.js";
+import voteSchema from "./vote-schema.js";
+
+const adminSchema = new Schema({
+  username: { type: String, required: true },
+
+  // TODO: Check if _id needs to be Schema.Types.ObjectId
+  _id: { type: Types.ObjectId, required: true },
+});
 
 const groupSchema = new Schema({
-  date: { type: Number, required: true },
+  date: { type: Date, required: true },
   groupName: { type: String, required: true },
   members: [userSchema],
   voting: [voteSchema],
-  admin: userSchema,
+  admin: { type: adminSchema },
 });
 
 export default model("Group", groupSchema);

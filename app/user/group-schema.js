2dc4c955eb1f03d20799cd3a873c79894c8c7181
Kelli Lilly <kelli.lilly05@gmail.com>
2023-02-07 09:43:45 -0600

    Delete files

--- app/user/group-schema.js
+++
@@ -1,8 +0,0 @@
-import { Schema } from "mongoose";
-import voteSchema from "./vote-schema.js";
-
-export default new Schema({
-  date: { type: Number, required: true },
-  groupName: { type: String, required: true },
-  voting: [voteSchema],
-});

6d211030b825551764963196d1efe6bac5ebb11e
Kelli Lilly <kelli.lilly05@gmail.com>
2023-02-07 13:53:32 -0600

    feat: :construction: update vote

--- app/group/controller.js
+++ app/group/controller.js
@@ -1,5 +1,6 @@
 import mongoose from "mongoose";
 import config from "../config.js";
+import controller from "../user/controller.js";
 import Group from "./Group.js";
 
 mongoose.set("strictQuery", true);
@@ -25,20 +26,24 @@ const groupController = {
   updateGroupMembers({ groupName, newMembers }) {
     return Group.findOneAndUpdate({ groupName }, { members: newMembers });
   },
+
+  async updateVoteTally({ groupName, newVoteTally }) {
+    const foundGroup = await Group.findOneAndUpdate(groupName);
+    if (foundGroup) {
+      foundGroup.voting.push(newVoteTally);
+      return foundGroup.save();
+    }
+  },
 };
 
-const groupMember = await groupController.updateGroupMembers({
+const newVote = await controller.updateVoteTally({
   groupName: "Group 1",
-  newMembers: [
-    {
-      name: "John Doe",
-    },
-    {
-      name: "Jane Doe",
-    },
-  ],
+  newVoteTally: {
+    voting: 1,
+    cuisineType: "Italian",
+  },
 });
 
-console.log(groupMember);
+console.log(newVote);
 
 export default groupController;

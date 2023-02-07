1132facc5f4dfa729c70ee67bbf413bb976fabf0
Kelli Lilly <kelli.lilly05@gmail.com>
2023-02-07 14:10:27 -0600

    chore: import cors to server

--- app/server.js
+++ app/server.js
@@ -1,5 +1,7 @@
+import cors from "cors";
 import express from "express";
 import config from "./config.js";
+import groupRoutes from "./group/routes.js";
 import decodeUser from "./middleware/decode-user.js";
 import routes from "./user/routes.js";
 
@@ -9,8 +11,9 @@ export default () => {
   // * Middleware order matters!
   app.use(express.json());
   app.use(decodeUser);
+  app.use(cors());
   app.use("/api/users", routes);
-  app.use("/api/groups");
+  app.use("/api/groups", groupRoutes);
 
   app.use((_, res) => {
     res.status(404).json({ message: "Not found" });

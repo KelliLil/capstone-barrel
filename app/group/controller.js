601b19171180a332ebb3f4783809a9139647edb5
Kelli Lilly <kelli.lilly05@gmail.com>
2023-02-08 09:41:44 -0600

    fix: :bug: Add new user to a group without replacing old members

--- app/group/controller.js
+++ app/group/controller.js
@@ -22,29 +22,46 @@ const groupController = {
     return Group.create(newGroup);
   },
 
-  updateGroupMembers({ groupName, newMembers }) {
-    return Group.findOneAndUpdate({ groupName }, { members: newMembers });
+  // Add a new member to the group
+  async updateGroupMembers({ groupName, newMember }) {
+    const groups = await this.getGroups();
+    const group2Update = groups.find((group) => group.groupName === groupName);
+
+    // TODO: Validate the new member is a valid user - confirm that the user exists in the user collection
+    // TODO: Validate that the new member is not already in the group
+
+    group2Update.members.push(newMember);
+    return group2Update.save();
   },
 
-  async updateVoteTally({ groupName, newVoteTally }) {
-    return Group.findOne({ name: "groupName" })
-      .then((group) => {
-        group.updateVoteTally(1);
-      })
-      .catch((error) => {
-        console.error(error);
-      });
-  },
+  // Update the vote tally for a group member
+  // async updateVoteTally(memberId, vote) {
+  //   try {
+  //     const item = await Group.members.findById(memberId);
+  //     if (item) throw new Error("Item not found");
+
+  //     // TODO: Update this in the schema to use this name
+  //     // TODO: Use push to add the new vote to the array
+  //     // TODO: Before pushing a new vote, check if the user has already voted for this category
+  //     // TODO: If the user has already voted, throw an error ("You have already voted for this category")
+  //     // TODO: If the category already exists, update the vote tally ELSE add a new category with a vote tally of 1
+  //     item.votes = item.votes + vote;
+  //     await item.save();
+  //   } catch (error) {
+  //     console.error(error);
+  //   }
+  // },
 };
 
-const newVote = await groupController.updateVoteTally({
+const addMember = await groupController.updateGroupMembers({
   groupName: "Group 1",
-  newVoteTally: {
-    voting: 1,
-    cuisineType: "Italian",
+  newMember: {
+    username: "user1",
+    name: "Please work",
+    _id: "63e279acf37f3ca25e980179",
   },
 });
 
-console.log(newVote);
+console.log(addMember);
 
 export default groupController;

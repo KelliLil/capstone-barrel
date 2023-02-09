94fe486f4ea38f025b590551f546bd49932b59aa
Kelli Lilly <kelli.lilly05@gmail.com>
2023-02-09 09:21:12 -0600

    Clean up Group Schema
    Use the correct Type for MongoId.
    Add versionKey: false

--- app/group/Group.js
+++ app/group/Group.js
@@ -1,17 +1,25 @@
-import { model, Schema, Types } from "mongoose";
+import { model, Schema } from "mongoose";
 import voteSchema from "./vote-schema.js";
 
-const newUserSchema = new Schema({
-  username: { type: String },
-  _id: { type: Types.ObjectId, required: true },
-});
+const newUserSchema = new Schema(
+  {
+    username: { type: String },
+    _id: { type: Schema.Types.ObjectId, required: true },
+  },
+  { _id: false }
+);
 
-const groupSchema = new Schema({
-  date: { type: Date, required: true },
-  groupName: { type: String, required: true },
-  members: [newUserSchema],
-  votes: [voteSchema],
-  admin: { type: newUserSchema },
-});
+const groupSchema = new Schema(
+  {
+    date: { type: Date, required: true },
+    groupName: { type: String, required: true },
+    members: [newUserSchema],
+    votes: [voteSchema],
+    admin: { type: newUserSchema },
+  },
+  {
+    versionKey: false,
+  }
+);
 
 export default model("Group", groupSchema);

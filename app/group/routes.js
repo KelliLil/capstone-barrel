4591acb0ebc2842026f0bec9e0777f67ede13af6
Kelli Lilly <kelli.lilly05@gmail.com>
2023-02-09 10:02:04 -0600

    feat: :sparkles: get route for all groups

--- app/group/routes.js
+++ app/group/routes.js
@@ -3,6 +3,11 @@ import groupController from "./controller.js";
 
 const router = new Router();
 
+router.get("/", (_, res) => {
+  groupController.getGroups().then((groups) => {
+    res.json(groups);
+  });
+});
 router.post("/group/update", async (req, res) => {
   groupController.updateGroupMembers(req.body).then((group) => {
     res.json(group);

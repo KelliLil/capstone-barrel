bfef9cbcb7dbf66d5a0e3ef3ddb0d7eed7eae0b5
Kelli Lilly <kelli.lilly05@gmail.com>
2023-02-09 11:12:50 -0600

    Wire PUT for voting route to controller

--- app/group/routes.js
+++ app/group/routes.js
@@ -14,4 +14,18 @@ router.post("/group/update", async (req, res) => {
   });
 });
 
+router.put("/:groupId/vote", async (req, res) => {
+  console.log("HELL");
+
+  const { groupId } = req.params;
+  const incomingVote = req.body;
+
+  const updatedGroup = await groupController.updateVoteTally(
+    groupId,
+    incomingVote
+  );
+
+  return res.status(200).json({ success: true, data: updatedGroup });
+});
+
 export default router;

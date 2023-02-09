a97908f1def8518d8a636b6a987b1f3636c9aaac
Kelli Lilly <kelli.lilly05@gmail.com>
2023-02-09 11:38:09 -0600

    docs:

--- app/group/controller.js
+++ app/group/controller.js
@@ -10,11 +10,9 @@ const groupController = {
   },
 
   createNewGroup({ newGroup }) {
-    // TODO: Validate the admin is a valid user - confirm that the user exists in the user collection
     return Group.create(newGroup);
   },
 
-  // Add a new member to the group
   async addNewMember2Group(groupId, newMember) {
     const group2Update = await Group.findById(groupId);
 
@@ -27,9 +25,7 @@ const groupController = {
     return group2Update.save();
   },
 
-  // Update the vote tally for a group member
   async updateVoteTally(groupId, incomingVote) {
-    // This is the group that the user is voting for
     const group = await this.getGroup(groupId);
     if (!group) throw new Error("Item not found");
 
@@ -37,7 +33,6 @@ const groupController = {
       (vote) => vote.cuisineType === incomingVote.cuisineType
     );
 
-    // If the votesCategory does not exist create it
     if (!votesCategory) {
       votesCategory = {
         cuisineType: incomingVote.cuisineType,
@@ -47,7 +42,6 @@ const groupController = {
       group.votes.push(votesCategory);
     }
 
-    // Otherwise, update the existing votesCategory
     const existingVoter = votesCategory.voters.find(
       (voter) => voter._id.toString() === incomingVote.memberId
     );
